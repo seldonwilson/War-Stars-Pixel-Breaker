e66ca7b9e79f2c4b6fda83c28963d8c7bc95c037
Danny W <seldon.wilson@gmail.com>
2019-04-25 12:52:46 -0700

    Paddle bounding now occurs programmitcally instead of being hard-coded.

--- Assets/Scripts/Paddle.cs
+++ Assets/Scripts/Paddle.cs
@@ -5,22 +5,26 @@ using UnityEngine;
 public class Paddle : MonoBehaviour
 {
    [SerializeField] float screenWidthUnits = -1.0f;
-   [SerializeField] float min = float.MinValue;
-   [SerializeField] float max = float.MaxValue;
+   [SerializeField] float min = float.MaxValue;
+   [SerializeField] float max = float.MinValue;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+      float width = gameObject.GetComponent<Collider2D>().bounds.size.x;
+      min = width / 2.0f;
+      max = screenWidthUnits - width / 2.0f;
+      Debug.Log("min is: " + min);
+      Debug.Log("max is: " + max);
     }
 
     // Update is called once per frame
     void Update()
     {
        float mouseXPosUnits = Input.mousePosition.x / Screen.width * screenWidthUnits;
-      Debug.Log(mouseXPosUnits);
        Vector2 paddlePos = new Vector2(transform.position.x, transform.position.y);
        paddlePos.x = Mathf.Clamp(mouseXPosUnits, min, max);
        transform.position = paddlePos;
+
     }
 }
