7f8e3dc97aa725ee5bb8f94eb449a813c0a5ec0e
Danny W <seldon.wilson@gmail.com>
2019-04-28 12:08:24 -0700

    Ball now launches on click. Potential feature to add: allow direction of paddle movement to cause initial vector to change.

--- Assets/Scripts/Ball.cs
+++ Assets/Scripts/Ball.cs
@@ -5,24 +5,48 @@ using UnityEngine;
 public class Ball : MonoBehaviour
 {
       // Config parameters
-   [SerializeField] Paddle paddle = null;
+   [SerializeField] Paddle  paddle;
+   [SerializeField] Vector2 startingVelocity;
 
       // State
-   Vector2 offset;
+   private Vector2     offset;
+   private Rigidbody2D m_rigidbody2d;
+   private bool        m_hasStarted;
 
     // Start is called before the first frame update
     void Start()
     {
+       m_hasStarted = false;
+       m_rigidbody2d = GetComponent<Rigidbody2D>();
+       m_rigidbody2d.simulated = false;
        offset = transform.position - paddle.transform.position;
+       
     }
 
     // Update is called once per frame
     void Update()
     {
-       Vector2 paddlePos = new Vector2(
-          paddle.transform.position.x, 
-          paddle.transform.position.y);
+       if (!m_hasStarted) {
+          LockBallToPaddle();
+          LaunchOnMouseClick();
+       }
+    }
 
-       transform.position = paddlePos + offset;
+   private void LockBallToPaddle()
+   {
+      Vector2 paddlePos = new Vector2(
+                paddle.transform.position.x,
+                paddle.transform.position.y);
+
+      transform.position = paddlePos + offset;
+   }
+
+   private void LaunchOnMouseClick()
+    {
+       if (Input.GetMouseButtonDown(0)) {
+          m_hasStarted = true;
+          m_rigidbody2d.simulated = true;
+          m_rigidbody2d.velocity = startingVelocity;
+       }
     }
 }
