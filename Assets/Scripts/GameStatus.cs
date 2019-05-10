2e407c5f5226c93c3bdb1f78f739ac509ccb6526
Danny W <seldon.wilson@gmail.com>
2019-05-09 21:42:39 -0700

    Added Game Status prefab to each level which current allows the game's speed to be changed from [0.1,10.0]. Eventually, this Game Status object will contain the information regarding the game state, such as the player's score, number of lives, etc.

---
+++ Assets/Scripts/GameStatus.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameStatus : MonoBehaviour
+{
+   [Range(0.1f, 10.0f)] [SerializeField] float gameSpeed;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        gameSpeed = 1.0f;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Time.timeScale = gameSpeed;
+    }
+}
