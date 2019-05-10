d057079cda4068ee7dad39994ea3e9b5b2a6cf4e
Danny W <seldon.wilson@gmail.com>
2019-05-09 20:43:14 -0700

    Added ability for blocks to decrement total block count in the level (but they do it by 10 for testing purposes). When the total number of blocks is less than or equal to 0, the level advances. The SceneLoader script was attached to the level prefab so level advancement works on all levels.

--- Assets/Scripts/Level.cs
+++ Assets/Scripts/Level.cs
@@ -4,10 +4,28 @@ using UnityEngine;
 
 public class Level : MonoBehaviour
 {
+      // Configuration Parameters
    [SerializeField] int breakableBlocks; // Serialized for debugging purposes
+
+      // Cached reference
+   SceneLoader sceneLoader;
+
+   private void Start()
+   {
+      sceneLoader = FindObjectOfType<SceneLoader>();
+   }
 
    public void CountBreakableBlocks()
    {
       breakableBlocks++;
+   }
+
+   public void BlockDestroyed()
+   {
+      breakableBlocks -= 10;
+
+      if (breakableBlocks <= 0) {
+         sceneLoader.LoadNextScene();
+      }
    }
 }
